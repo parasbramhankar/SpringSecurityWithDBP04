abb9993a8d063a766da9e01b1b27c356499bbba2
parasbramhankar <parasbramhankar.in@gmail.com>
2025-12-31 16:45:15 +0530

    commit

--- src/main/java/com/example/SpringSecurityWithDBP04/Config/WebConfig.java
+++ src/main/java/com/example/SpringSecurityWithDBP04/Config/WebConfig.java
@@ -3,14 +3,20 @@ package com.example.SpringSecurityWithDBP04.Config;
 import com.example.SpringSecurityWithDBP04.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class WebConfig {
 
     @Autowired
