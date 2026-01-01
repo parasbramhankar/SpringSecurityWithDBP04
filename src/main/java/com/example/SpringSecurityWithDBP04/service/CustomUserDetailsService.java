bc4b2f12acc5a2e7b2638a502764e27d122c67d4
parasbramhankar <parasbramhankar.in@gmail.com>
2026-01-01 10:11:29 +0530

    commit

--- src/main/java/com/example/SpringSecurityWithDBP04/service/CustomUserDetailsService.java
+++ src/main/java/com/example/SpringSecurityWithDBP04/service/CustomUserDetailsService.java
@@ -18,7 +18,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        UserEntity user=userRepo.findByUsername(username).orElseThrow(()->new UsernameNotFoundException(" User not found"));
+        UserEntity user=userRepo.findByUsername(username)
+                .orElseThrow(()->new UsernameNotFoundException(" User not found"));
 
         return User.builder()
                 .username(user.getUsername())
